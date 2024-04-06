a8b4414f95399f4917758f645ba41a1e6abf6cb8
avicos <adapaavinash612@gmail.com>
2024-04-06 15:35:51 +0530

    changes in backend

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+'mongodb+srv://austenlance6:root@books-store-mern.8yzblg2.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
